5918f4fcdd1d9c43921455266dfd19eaa498fd2c
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-15 10:17:40 -0500

    setup get request and initial post request

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,54 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// IMPORT DATA
+const userDb = require("./userDb");
+const postDb = require("../posts/postDb");
+
+router.post("/", validateUser, (req, res) => {
+  // do your magic!
+  userDb
+    .insert(req.body)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      });
+    });
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  userDb
+    .get(req.query)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error retrieving data." });
+    });
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
